a8961f2c4adc22ec2ba40101bfad6b4407cdcd42
dharinibodar3010 <dharinibodar3010@gmail.com>
2025-09-20 21:10:32 +0530

    Add files via upload

---
+++ 09-09-2025/main (18).c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main ()
+{
+    int a = 7;
+    
+    // unary operator
+    
+    int c = a--; 
+    
+    printf("\n %d", a);
+    printf("\n %d", c);
+    
+    return 0;
+}
