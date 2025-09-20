c4e9fae2fead2632ab8c64a08c0bd14e1d63ac5b
dharinibodar3010 <dharinibodar3010@gmail.com>
2025-09-20 21:58:39 +0530

    commit

---
+++ 11-09-2025/main (21).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+int main ()
+{
+    char ch;
+    
+    //print first character of name ex - Dharini -> D
+    
+    printf("Enter Your Name : ");
+    scanf("%c", &ch);
+    
+    printf("Your Name is : %c",ch);
+    
+    return 0;
+}
