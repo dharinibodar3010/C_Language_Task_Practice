4eca3fc1577b3c40f8daea2d82aed9f71f3301cb
dharinibodar3010 <dharinibodar3010@gmail.com>
2025-10-05 15:54:05 +0530

    commit

--- Extra Lab Exercises/8)  Check Number Properties.c
+++ Extra Lab Exercises/8)  Check Number Properties.c
@@ -1,4 +1,12 @@
 #include<stdio.h>
+
+/*LAB EXERCISE 2: Check Number Properties
+Que: Write a C program that takes an integer from the user and checks the following using
+different operators:
+o Whether the number is even or odd.
+o Whether the number is positive, negative, orzero.
+o Whether the number is a multiple of both 3 and 5.*/
+    
 int main ()
 {
     int num;
