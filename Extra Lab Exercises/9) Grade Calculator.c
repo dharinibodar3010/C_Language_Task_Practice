56ebb021c0502c1e8cb7b334644ba4d5377ada8f
dharinibodar3010 <dharinibodar3010@gmail.com>
2025-10-05 15:56:06 +0530

    commit

--- Extra Lab Exercises/9) Grade Calculator.c
+++ Extra Lab Exercises/9) Grade Calculator.c
@@ -1,4 +1,14 @@
 #include<stdio.h>
+
+/*LAB EXERCISE 1: Grade Calculator
+Que: Write a C program that takes the marks of a student as input and displays the corresponding
+grade based on the following conditions:
+o Marks > 90: Grade A
+o Marks > 75 and <= 90: Grade B
+o Marks > 50 and <= 75: Grade C
+o Marks <= 50: Grade D
+Use if-else orswitch statements for the decision-making process.*/
+    
 int main ()
 {
     int marks;
