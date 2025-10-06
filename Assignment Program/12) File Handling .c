69e7f553a80e736f9693c405967aa1da542ca0b5
dharinibodar3010 <dharinibodar3010@gmail.com>
2025-10-06 19:04:55 +0530

    commit

--- Assignment Program/12) File Handling .c
+++ Assignment Program/12) File Handling .c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 
+/*LAB EXERCISE:
+o Write a C program to create a file, write a string into it, close the file, then
+open the file again to read and display its contents.*/
+
 int main()
 {
     FILE *fp;
