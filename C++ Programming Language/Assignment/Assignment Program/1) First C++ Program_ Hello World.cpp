319f607298d5626934a790d79b09018af2412d9b
dharinibodar3010 <dharinibodar3010@gmail.com>
2025-10-28 21:37:28 +0530

    commit

---
+++ C++ Programming Language/Assignment/Assignment Program/1) First C++ Program_ Hello World.cpp
@@ -0,0 +1,15 @@
+/*Que 1: First C++ Program: Hello World
+o Write a simple C++ program to display "Hello, World!".
+o Objective: Understand the basic structure of a C++ program, including #include,
+main(), and cout.*/
+
+#include <iostream>   // Header file for input/output
+
+using namespace std;  // Allows using cout, cin without std::
+
+int main() // where program execution starts
+{
+    cout << "Hello, World!";  // Display message on the screen
+    
+    return 0;                 // End of program
+}
