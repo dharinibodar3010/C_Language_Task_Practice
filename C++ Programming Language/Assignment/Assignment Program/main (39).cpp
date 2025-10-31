0d13a3a5cb27e32a161b47e289f69acc033fb447
dharinibodar3010 <dharinibodar3010@gmail.com>
2025-10-31 19:44:36 +0530

    commit

---
+++ C++ Programming Language/Assignment/Assignment Program/main (39).cpp
@@ -0,0 +1,35 @@
+/*3. String Palindrome Check
+o Write a C++ program to check if a given string is a palindrome (reads the same
+forwards and backwards).
+o Objective: Practice string operations.
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string str, rev = "";
+    
+    cout << "Enter a string: ";
+    cin >> str; // input a single word
+    
+    // Reverse the String 
+    for(int i = str.length() - 1; i >= 0; i--)
+    {
+        rev += str[i];
+    }
+    
+    // Check if palindrome
+    if (str == rev)
+    {
+        cout << "This string is a palidrome." << endl;
+    }
+    else 
+    {
+        cout << "This string is not palidrome." << endl;
+    }
+    
+    return 0;
+    
+}
