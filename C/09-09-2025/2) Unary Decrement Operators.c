c89af7a32e6b03a34a3c152c60f566007bb724dd
dharinibodar3010 <dharinibodar3010@gmail.com>
2025-12-27 21:52:01 +0530

    commit

---
+++ C/09-09-2025/2) Unary Decrement Operators.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main ()
+{
+    int a = 7;
+    
+    // unary decrement operator
+    
+    int c = a--; 
+    
+    printf("\n %d", a);
+    printf("\n %d", c);
+    
+    return 0;
+}
