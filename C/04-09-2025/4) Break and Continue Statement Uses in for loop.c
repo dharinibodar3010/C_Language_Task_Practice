dbbfb2995e89424dc806e63b5cd6a3ea21357d01
dharinibodar3010 <dharinibodar3010@gmail.com>
2025-12-27 21:48:35 +0530

    commit

---
+++ C/04-09-2025/4) Break and Continue Statement Uses in for loop.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ()
+{
+    for(int i = 1; i <= 10; i++)
+    {
+        if(i==5)
+        {
+        //break;    
+        
+        continue;   
+        }
+        printf("\n %d",i);
+    }
+
+    return 0;
+}
